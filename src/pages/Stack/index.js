c453dd09e39cc463f23cda20def63c9773e5a519
Vinicius Braz <vinicius_lsb@live.com>
2022-07-10 15:42:43 -0300

    feat: implements myStack layout

--- src/pages/Stack/index.js
+++ src/pages/Stack/index.js
@@ -1,7 +1,23 @@
-// import styles from './Stack.module.scss';
+import styles from './Stack.module.scss';
+import stackIcons from '../../data/stackIcons';
 
 export default function Stack() {
   return (
-    'My Stack'
+    <div className={styles.container}>
+      <div className={styles.stackGrid}>
+        {stackIcons.map(({ name, icon, color }) => (
+          <div
+            key={name}
+            style={{ opacity: 1, color }}
+            className={styles.stackIcon}
+          >
+            {icon}
+            <span>
+              {name}
+            </span>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
